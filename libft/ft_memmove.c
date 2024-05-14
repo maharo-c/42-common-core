981c69fd247e335a7c84a354a4e6ee119729d511
Margarita Haro Chica <maharo-c@c3r1s5.42malaga.com>
2024-05-14 15:22:02 +0200

    Se corrigen fallos

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: maharo <maharo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 15:59:39 by maharo            #+#    #+#             */
-/*   Updated: 2024/05/14 13:29:07 by maharo           ###   ########.fr       */
+/*   Updated: 2024/05/14 15:20:11 by maharo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char		*dst_ptr;
 	const char	*src_ptr;
 
+	if (dst == NULL && src == NULL && len > 0)
+	{
+		return (NULL);
+	}
 	dst_ptr = (char *)dst;
 	src_ptr = (const char *)src;
 	if (dst_ptr <= src_ptr || src_ptr >= (dst_ptr + len))
 	{
 		while (len--)
-		{
 			*dst_ptr++ = *src_ptr++;
-		}
 	}
 	else
 	{
 		dst_ptr += len - 1;
 		src_ptr += len - 1;
 		while (len--)
-		{
 			*dst_ptr-- = *src_ptr--;
-		}
 	}
 	return (dst);
 }
