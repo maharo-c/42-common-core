0d2181ee79df77e030a3de4e9c1457f0a81aab30
maharo-c <margarita.haro@hotmail.com>
2024-05-23 06:04:50 +0000

    añado más funciones

---
+++ ft_strlcpy.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
